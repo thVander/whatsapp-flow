a60ce51b0e72ad32fa9259f9a97577b66e8c722f
Thiago Eduardo Vanderlinde <thiago.vanderlinde@br.com.acgsa>
2025-08-19 07:59:25 -0300

    teste

--- src/flow.js
+++ src/flow.js
@@ -239,6 +239,7 @@ Phone: ${data.phone}
             extension_message_response: {
               params: {
                 flow_token,
+                "teste": "teste"
               },
             },
           },
